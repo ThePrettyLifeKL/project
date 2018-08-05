8210ca9ffd102d883b2c0de3635e15de1e6d6c34
ThePrettyLifeKL <1973488043@qq.com>
2018-08-05 19:17:44 +0800

    代码提交流程

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 # PM修改代码
-
+# PM修改代码
+# 流程:add--->Commit--->Push
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
